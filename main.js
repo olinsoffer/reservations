9d149fedd30adc7601f09c8cbbece0db51545c7f
Olin Soffer <olinsoffer@outlook.com>
2017-08-23 09:53:24 +0300

    minor

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
-var reservations = {
+let reservations = {
   'Bob': { claimed: false },
   'Ted': { claimed: true }
 }
 
-var name = prompt('Please enter the name for your reservation');
+let resName = prompt('Please enter the name for your reservation');
 
-var claimReservation = function () {
-  // write your code here!
+let claimReservation = function() {
+  formatedName = formatName(resName);
+  if(reservations[formatedName] === undefined) {
+    alert("There is nothing currenty under your name");
+    reservations[formatedName] = {claimed: true};
+    alert("You now have a new reservation!");
+  } else if(reservations[formatedName].claimed === false) {
+    alert(`Welcome ${name}`);
+  } else if(reservations[formatedName].claimed === true) {
+    alert("Your reservation was already claimed");
+  }
+
+  function formatName(nameToFormat) {
+     let capLetter = nameToFormat[0].toUpperCase(),
+     restOfName = nameToFormat.slice(1).toLowerCase(),
+     newName = capLetter+restOfName;
+     return newName;
+  }
 }
 
 claimReservation();
